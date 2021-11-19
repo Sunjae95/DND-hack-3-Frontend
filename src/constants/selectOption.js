32dec2ee3864ad0791e158dfda02996521b10808
Sunjae95 <tjswo9502@gmail.com>
2021-11-20 08:02:34 +0900

    chore: 상수추가

---
+++ src/constants/selectOption.js
@@ -0,0 +1,12 @@
+export const gradeOptions = [
+  { name: '티어', value: '전체' },
+  { name: '아이언', value: '전체' },
+  { name: '브론즈', value: '전체' },
+  { name: '실버', value: '전체' },
+  { name: '골드', value: '전체' },
+  { name: '플레티넘', value: '전체' },
+  { name: '다이아', value: '전체' },
+  { name: '그랜드마스터', value: '전체' },
+  { name: '마스터', value: '전체' },
+  { name: '챌린저', value: '전체' },
+];
