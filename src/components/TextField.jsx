62420fe2d9ccd5920f8fd30e584b683951c2fdff
Sunjae95 <tjswo9502@gmail.com>
2021-11-20 06:42:13 +0900

    refactor: TextField

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 import { colors } from '../assets/colors';
 
 export function TextField(props) {
-  return <Input type="text" {...props} />;
+  return <Input type="text" {...props}></Input>;
 }
 
 export const Input = styled.input`
-  border: none;
-  padding: 5px;
+  border: 1px solid ${colors.opacityDark};
+  width: 100%;
+  height: 40px;
+  padding: 8px;
+  border-radius: 8px;
   font-size: 16px;
+  box-sizing: border-box;
   font-weight: bold;
-
+  outline: none;
   &::placeholder {
-    color: ${colors.grey[1]};
+    color: ${colors.opacityDark};
   }
 `;
