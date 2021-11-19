2c22ca8392d4a50b5997bddb75d983a06da79418
jinsunee <jsunee.dev@gmail.com>
2021-11-20 02:27:13 +0900

    feat: SelectButton 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Button } from '@components/Button';
+import { SelectButton } from '@components/SelectButton';
 import { TextButton } from '@components/TextButton';
 import { TextField } from '@components/TextField';
 import React from 'react';
@@ -9,6 +10,7 @@ function App() {
       <Button>123</Button>
       <TextButton>123</TextButton>
       <TextField placeholder="1" />
+      <SelectButton options={[{ name: '123', value: '123' }]} />
     </div>
   );
 }
