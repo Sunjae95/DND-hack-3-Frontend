9b250fa4441760cb1655ee91f7bead0f23247383
Sunjae95 <tjswo9502@gmail.com>
2021-11-19 16:20:03 +0900

    chore: init

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  babel: {
+    presets: ['@emotion/babel-preset-css-prop'],
+  },
+  webpack: {
+    alias: {
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@router': path.resolve(__dirname, 'src/router'),
+    },
+  },
+};
