a5d0383a1b70e698682a671c0714e0eca3866280
jinsunee <jsunee.dev@gmail.com>
2021-11-20 05:15:50 +0900

    fix: responsive frame만 추가

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -1,6 +1,6 @@
 export const colors = {
-  primary: '#4448D5',
+  primary: '#0137F3',
   white: '#fff',
   dark: '#000',
-  grey: ['#f2f2f2', '#E0E0E0'],
+  grey: ['#f2f2f2', '#E0E0E0', '#D5D8E2', '#E9EBF2'],
 };
--- src/styles/responsive.js
+++ src/styles/responsive.js
@@ -1,11 +1,12 @@
+import { mobileSize } from '@constants/screenSize';
 import { useMediaQuery } from 'react-responsive';
 
 export const Desktop = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 361 });
+  const isDesktop = useMediaQuery({ minWidth: mobileSize });
   return isDesktop ? children : null;
 };
 
 export const Mobile = ({ children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 360 });
+  const isMobile = useMediaQuery({ maxWidth: mobileSize });
   return isMobile ? children : null;
 };
