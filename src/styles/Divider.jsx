866803c96b53705ef8d2b4953585dcf97645d1b7
jinsunee <jsunee.dev@gmail.com>
2021-11-20 14:12:56 +0900

    feat: 링크복사 기능 구현

--- src/styles/Divider.jsx
+++ src/styles/Divider.jsx
@@ -4,5 +4,5 @@ import { colors } from '../assets/colors';
 export const Divider = styled.div`
   width: 100%;
   height: 1px;
-  color: ${colors.grey[1]};
+  background-color: ${colors.grey[1]};
 `;
